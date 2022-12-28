3f921f03f7b5a769e7ce9e077d2eedd5f2c772d6
Piotr Duda <piotr.duda00@gmail.com>
2022-12-28 20:27:43 +0100

    fix

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,8 +9,7 @@ import {
     MDBRow,
     MDBCol,
     MDBCardImage
-}
-    from 'mdb-react-ui-kit';
+} from 'mdb-react-ui-kit';
 import Button from "react-bootstrap/Button";
 import {Link} from "react-router-dom";
 
