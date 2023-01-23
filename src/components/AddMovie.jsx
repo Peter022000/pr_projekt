721602bf2f040783482e645a2bf81bb92fe84bd8
Piotr Duda <piotr.duda00@gmail.com>
2023-01-23 15:02:55 +0100

    Add movie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../App.css';
 import Footer from "./Footer";
 import {
@@ -9,9 +9,68 @@ import {
 }
     from 'mdb-react-ui-kit';
 import Button from "react-bootstrap/Button";
+import {useNavigate} from "react-router-dom";
 
 
 const AddMovie = (props) => {
+
+    const [title, setTitle] = useState('');
+    const [titlePlaceHolder, setTitlePlaceHolder] = useState('Wprowadź tytuł');
+    const [image, setImage] = useState('');
+    const [imagePlaceHolder, setImagePlaceHolder] = useState('Wprowadź link do obrazu');
+    const [description, setDescription] = useState('');
+    const [descriptionPlaceHolder, setDescriptionPlaceHolder] = useState('Wprowadź opis');
+
+    const navigate = useNavigate();
+
+    const validate = () => {
+        let status = true;
+        if (title === '') {
+            alert("Wprowadź tytuł!")
+            status = false;
+        } else if (image === '') {
+            alert("Wprowadź link do obrazu!")
+            status = false;
+        } else if (description === '') {
+            alert("Wprowadź opis!")
+            status = false;
+        }
+
+        return status;
+    }
+
+    const send = async () => {
+
+        const options = {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                title: title,
+                image: image,
+                content: description
+            })
+        }
+
+        await fetch('https://at.usermd.net/api/movies', options)
+            .then(response => {
+                if (response.ok) {
+                    alert("Dodano film");
+                    navigate("/");
+                } else {
+                    response.text().then(r => alert(r))
+                }
+            })
+            .catch(error => console.log( error ));
+    }
+
+    const addMovie = () => {
+        if(validate()){
+            send().then(r => {});
+        }
+    }
 
     return (
         <div className="page-container">
@@ -21,12 +80,10 @@ const AddMovie = (props) => {
                         <MDBCardBody>
                             <MDBRow style={{justifyContent: "center", alignItems: "center", alignContent: "center"}}>
                                 <MDBCol md='10' lg='6' className='d-flex flex-column'>
-                                    <MDBInput wrapperClass='mb-4' label='Tytuł' id='form1' type='text'/>
-                                    <MDBInput wrapperClass='mb-4' label='Link do obrazu' id='form1' type='text'/>
-                                    <MDBInput wrapperClass='mb-4' label='Tytuł oryginalny' id='form2' type='text'/>
-                                    <MDBInput wrapperClass='mb-4' label='Ocena' id='form2' type='number'/>
-                                    <MDBTextArea label='Opis' rows={6}></MDBTextArea>
-                                    <Button style={{marginTop: 12}} className="mb-4">Zapisz</Button>
+                                    <MDBInput placeholder={titlePlaceHolder} value={title} onChange={e => setTitle(e.target.value)} wrapperClass='mb-4'  label='Tytuł' id='form1' type='text'/>
+                                    <MDBInput placeholder={imagePlaceHolder} value={image} onChange={e => setImage(e.target.value)} wrapperClass='mb-4' label='Link do obrazu' id='form1' type='text'/>
+                                    <MDBTextArea placeholder={descriptionPlaceHolder} value={description} onChange={e => setDescription(e.target.value)} label='Opis' rows={6}></MDBTextArea>
+                                    <Button style={{marginTop: 12}} onClick={() => addMovie()} className="mb-4">Zapisz</Button>
                                 </MDBCol>
                             </MDBRow>
                         </MDBCardBody>
