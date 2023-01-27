5e5ce736aeb6a2752bb42523e54cb3bf19710fe6
Piotr Duda <piotr.duda00@gmail.com>
2023-01-27 22:48:55 +0100

    new style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,11 @@
 import React from "react"
-import {Outlet} from "react-router-dom";
+import '../App.css';
 
 const Footer = () => {
     return (
-        <>
-            <div className="main-footer">
-                <div style={{backgroundColor: "#F8F9FA"}} className="text-center py-3">
-                    © {new Date().getFullYear()} Copyright: Piotr Duda
-                </div>
-            </div>
-        </>
+        <div className={"footer"}>
+            © {new Date().getFullYear()} Copyright: Piotr Duda
+        </div>
     )
 }
 
