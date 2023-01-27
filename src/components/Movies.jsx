dc5a94d65888cfcb8fbc1041b7b0920b493317bd
Piotr Duda <piotr.duda00@gmail.com>
2023-01-27 18:50:09 +0100

    fix_view

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -5,7 +5,7 @@ import {useLocation} from "react-router-dom";
 const Movies = (props) => {
 
     return (
-        <div style={{display: "flex", flexWrap:"wrap", justifyContent: "center" }}>
+        <div style={{display: "flex", flexWrap:"wrap", justifyContent: "center", padding: "1rem"}}>
             {props.movies.map((movie, index) => (
                 <MovieCard movie={movie} />
             ))}
