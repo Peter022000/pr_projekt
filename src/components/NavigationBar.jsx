156527427d60f3d78a6ef1b0793d261fe0b06731
Piotr Duda <piotr.duda00@gmail.com>
2023-02-02 17:20:53 +0100

    Update NavigationBar.jsx

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -32,64 +32,62 @@ const NavigationBar = () => {
     }
 
     return (
-        <>
-            <Navbar bg="light" expand="lg" style={{height:'4rem'}}>
-                <Container fluid>
-                    <Navbar.Brand>
-                        <Link className="text-decoration-none text-black" to="/">
-                            Filmoteka
-                        </Link>
-                    </Navbar.Brand>
-                    <Navbar.Toggle style={{marginBottom:'1rem'}} aria-controls="navbarScroll" />
-                    <Navbar.Collapse style={{backgroundColor: "#f8f9fa", zIndex:1000}} id="navbarScroll">
-                        <Nav
-                            className="me-auto my-2 my-lg-0"
-                            navbarScroll
-                        >
-                            <Nav.Link>
-                                <Link className="text-decoration-none text-black" to="/">
-                                    Strona główna
-                                </Link>
-                            </Nav.Link>
-                            {isNotLogged && <Nav.Link>
-                                <Link className="text-decoration-none text-black" to="/signin">
-                                    Logowanie
-                                </Link>
-                            </Nav.Link>}
-                            {isNotLogged && <Nav.Link>
-                                <Link className="text-decoration-none text-black" to="/signup">
-                                    Rejestracja
-                                </Link>
-                            </Nav.Link>}
+        <Navbar bg="light" expand="lg" style={{height:'4rem'}}>
+            <Container fluid>
+                <Navbar.Brand>
+                    <Link className="text-decoration-none text-black" to="/">
+                        Filmoteka
+                    </Link>
+                </Navbar.Brand>
+                <Navbar.Toggle style={{marginBottom:'1rem'}} aria-controls="navbarScroll" />
+                <Navbar.Collapse style={{backgroundColor: "#f8f9fa", zIndex:1000}} id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        navbarScroll
+                    >
+                        <Nav.Link>
+                            <Link className="text-decoration-none text-black" to="/">
+                                Strona główna
+                            </Link>
+                        </Nav.Link>
+                        {isNotLogged && <Nav.Link>
+                            <Link className="text-decoration-none text-black" to="/signin">
+                                Logowanie
+                            </Link>
+                        </Nav.Link>}
+                        {isNotLogged && <Nav.Link>
+                            <Link className="text-decoration-none text-black" to="/signup">
+                                Rejestracja
+                            </Link>
+                        </Nav.Link>}
 
-                            {!isNotLogged && <Nav.Link>
-                                <Link className="text-decoration-none text-black" to="/add">
-                                    Dodaj film
-                                </Link>
-                            </Nav.Link>}
+                        {!isNotLogged && <Nav.Link>
+                            <Link className="text-decoration-none text-black" to="/add">
+                                Dodaj film
+                            </Link>
+                        </Nav.Link>}
 
-                            {!isNotLogged && <Nav.Link>
-                                <Link onClick={() => {localStorage.removeItem('token'); window.location.href = "/";}} className="text-decoration-none text-black" to="/">
-                                    Wyloguj się
-                                </Link>
-                            </Nav.Link>}
+                        {!isNotLogged && <Nav.Link>
+                            <Link onClick={() => {localStorage.removeItem('token'); window.location.href = "/";}} className="text-decoration-none text-black" to="/">
+                                Wyloguj się
+                            </Link>
+                        </Nav.Link>}
 
-                        </Nav>
-                        <Form className="d-flex" onSubmit={handleSubmit}>
-                            <Form.Control
-                                type="search"
-                                placeholder=""
-                                className="me-2"
-                                aria-label="Search"
-                                value={phrase}
-                                onChange={e => setPhrase(e.target.value)}
-                            />
-                            <Button onClick={() => search()} variant="outline-success">Wyszukaj</Button>
-                        </Form>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
+                    </Nav>
+                    <Form className="d-flex" onSubmit={handleSubmit}>
+                        <Form.Control
+                            type="search"
+                            placeholder=""
+                            className="me-2"
+                            aria-label="Search"
+                            value={phrase}
+                            onChange={e => setPhrase(e.target.value)}
+                        />
+                        <Button onClick={() => search()} variant="outline-success">Wyszukaj</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
 
